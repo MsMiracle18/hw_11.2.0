58d59c112eef585245b9f0ab4cb7dd57a8c89b61
Lera <128050534+MsMiracle18@users.noreply.github.com>
2023-10-07 17:55:36 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://msmiracle:password@localhost/dbname"
+DATABASE_URL = "postgresql://msmiracle:ms_miracle18@localhost/4274495_ranking"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
